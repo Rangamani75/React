4cdad933eaa1a4f948eb90ac5fc872e8e55fdc54
Rangamani75 <rangamaniranganathappagari@gmail.com>
2025-09-10 23:18:07 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Search from "./components/Search.jsx";
-import Weather from "./components/Weather.jsx";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Routes,Route} from "react-router-dom"
+import Product from './Product'
+import Home from './Home'
 
 function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: <Search /> },
-    { path: "/weather/:city", element: <Weather /> }
-  ]);
+  
 
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <h1>WELCOME TO PRODUCT STORE APP</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/product/:id" element={<Product/>}/>
+      </Routes>
+
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,80 +1,107 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import {useState,useEffect} from "react"
 import { Link } from "react-router-dom";
 
 export default function Home(){
-	let [search,setSearch] = useState("");
 
-let [state,setState] = useState({data:[],loading:false,error:null});
-  const [originalData, setOriginalData] = useState([]);
 
-async function fetchPosts(){
+	let [filter,setFilters] = useState({
+		sort:"Sort By",filter:"all"
+	})
 
-	setState({...state,loading:true})
-	try{
-		let res = await fetch("https://dummyjson.com/posts");
+	let [data,setData] = useState({
+		products:[],loading:false,error:null
+	})
+
+
+	function handleChange(e){
+		let {name,value} = e.target;
+
+		setFilters({...filter,[name]:value});
+		
+
+
+	}
+
+	function filterAndSort(){
+		let filtered = [...data.products]
+		if(!filter.sort || !filter.filter) return alert("Not a avalid filter")
+		 filtered = filtered.filter((ele)=>{
+	         if(filter.filter==="all") return true;
+			 else return filter.filter===ele.category.toLowerCase()
+		})
+		filtered.sort((a,b)=>{
+			if (filter.sort === "Sort By") return;
+
+			if(filter.sort==="high") return b.price-a.price;
+			else return a.price-b.price;
+		})
+		
+setAllData(filtered)
+		
+	}
+	useEffect(() => {
+  filterAndSort();
+}, [filter.sort,filter.filter,data.products]);
+
+
+
+
+	let [allData,setAllData] = useState([]);
+
+	async function fetchProducts(){
+		setData({...data,loading:true})
+		try{
+           let res = await fetch("https://dummyjson.com/products");
 		let dat = await res.json();
-		      setOriginalData(dat.posts); // store original data
-
-		setState({...state,data:dat.posts,loading:false})
 		console.log(dat)
+		setData({...data,loading:false,products:dat.products})
+		setAllData(dat.products)
+		}
+		catch(err){
+			setData({...data,error:err.message})
+		}
 	}
-	catch(err){
-		setState({...state,error:err.message});
-	}
-}
+ useEffect(()=>{
+	fetchProducts();
+ },[])
 
-useEffect(()=>{
-	fetchPosts();
-},[])
+return(
+	<>
 
-//let filtered = []
-function handleChange(e){
-	let val = e.target.value;
-	console.log(val)
-	setSearch(val);
+<div>
+	<select name="filter" value={filter.filter} onChange={handleChange}>Filter By
+		<option value="all">ALL</option>
+		<option value="beauty">Beauty</option>
+		<option value="groceries">Groceries</option>
+		<option value="fragrances">Fragrances</option>
+		<option value="furniture">Furniture</option>
+	</select>
+	<select name="sort" value={filter.sort} onChange={handleChange}>Sort By
+		<option value="Sort By">Sort By</option>
+		<option value="low">low-high</option>
+		<option value="high">high-low</option>
+	</select>
+</div>
 
-	let filtered = originalData.filter((ele)=> ele.title.toLowerCase().includes(val.toLowerCase()))
-
-	setState((prev)=>({...prev,data:filtered}));
-}
-// useEffect(()=>{
-// 	handleChange()
-// },[search])
-
-
-
-
-
-	return(
-		<>
-
-		<div>
-
-<input type="text" placeholder="Search for a title" value={search} onChange={handleChange} />
-<br />
-<br />
-<h3>Your Posts.....</h3>
-{state.loading && <h3>Loading....</h3>}
-{state.error && <h3>Error:{state.error}</h3>}
-{state.data && state.data.map((ele)=>(
-  <div key={ele.id}>
-        <h3>{ele.title}</h3>
-		<p>Likes❤️:{ele.reactions.likes}</p>
-		<ul>Tags:  {ele.tags.map((el)=>(
-			<>{el+"   "}</>
-		))}</ul>
-
-		<Link to={`/post/${ele.id}`}>View More</Link>
+      {data.loading && <h3>Loading.....</h3>}
+	  {data.error && <h3>Error:{data.error}</h3>}
+	  {allData.length>0 && <div>
+		{
+			allData.map((ele)=>(
+				<div key={ele.id}>
+					<h3>{ele.title}</h3>
+					<p>{ele.description}</p>
+					<p>category:{ele.category}</p>
+					<p><b>Price:</b>{ele.price}</p>
+					<Link to={`/product/${ele.id}`}>View More</Link>
+				</div>
+			))
+		}
+		</div>}
 
 
-  </div>
-))}
+	</>
+)
 
-
-		</div>
-
-
-
-		</>
-	)
 }
